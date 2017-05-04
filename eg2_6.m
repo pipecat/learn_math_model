409cd8ee9e8749b6ed60faaeb63a2d5139337672
Runyuan Zhao <Runyuan Zhao>
2017-05-05 00:20:07 +0800

    first commit

---
+++ eg2_6.m
@@ -0,0 +1,15 @@
+clear;
+close;
+xa = -2:0.2:2;
+ya=xa;
+[x, y] = meshgrid(xa, ya);
+z = x.*exp(-x.^2-y.^2);
+mesh(x, y, z);
+pause
+msurf(x, y, z);
+pause
+contour(x, y, z);
+contour(x, y, z, [0.1, 0.1]);
+pause
+mesh(x, y, z);
+
---
+++ test_m.m
@@ -0,0 +1,7 @@
+clear;
+s=0;
+for n = 1:100
+    s = s+1/n/n;
+end
+s
+
